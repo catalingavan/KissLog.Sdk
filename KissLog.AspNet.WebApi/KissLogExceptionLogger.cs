5ad78c7657b4d7cd01c2ff03f015e46044d84cad
catalin.gavan <catalin.gavan@additiv.ch>
2018-05-15 12:20:10 +0300

    created KissLogExceptionLogger, helping catch more unhandled exceptions

---
+++ KissLog.AspNet.WebApi/KissLogExceptionLogger.cs
@@ -0,0 +1,29 @@
+﻿using KissLog.AspNet.Web;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace KissLog.AspNet.WebApi
+{
+    public class KissLogExceptionLogger : ExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            ILogger logger = LoggerFactory.GetInstance();
+            logger.Error(context.Exception);
+        }
+
+        public override Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            ILogger logger = LoggerFactory.GetInstance();
+            logger.Error(context.Exception);
+
+            return Task.FromResult(true);
+        }
+
+        public override bool ShouldLog(ExceptionLoggerContext context)
+        {
+            return true;
+        }
+    }
+}
