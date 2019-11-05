a0646ef425ff7fc471a3bf70cc0b7bad84dd5b19
Catalin Gavan <catalingavan@gmail.com>
2019-11-05 17:08:54 +0200

    Update readme (#21)
    * updating readme

--- src/KissLog/TemporaryFile.cs
+++ src/KissLog/TemporaryFile.cs
@@ -7,7 +7,50 @@ namespace KissLog
     {
         public string FileName { get; private set; }
 
-        public TemporaryFile() : this(Path.GetTempFileName())
+        private static string GenerateSalt()
+        {
+            return Guid.NewGuid().ToString("N").Substring(0, 12);
+        }
+
+        private static string GetTempFileName()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "KissLog", $"{GenerateSalt()}.tmp");
+
+            try
+            {
+                FileInfo fi = new FileInfo(path);
+                fi.Directory.Create();
+
+                using (File.Create(path)) { };
+            }
+            catch
+            {
+                path = null;
+            }
+
+
+            if (path == null)
+            {
+                try
+                {
+                    path = Path.Combine(Path.GetTempPath(), $"KissLog_{GenerateSalt()}.tmp");
+                    using (File.Create(path)) { };
+                }
+                catch
+                {
+                    path = null;
+                }
+            }
+
+            if (path == null)
+            {
+                path = Path.GetTempFileName();
+            }
+
+            return path;
+        }
+
+        public TemporaryFile() : this(GetTempFileName())
         {
         }
 
