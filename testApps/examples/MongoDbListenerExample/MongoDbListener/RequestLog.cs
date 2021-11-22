3edad5244c48e61af261d6ecc7f1929675d8ed02
Catalin Gavan <catalingavan@gmail.com>
2021-11-22 17:13:38 +0100

    Mongodb example (#51)
    * implemented CustomMongoDbListener listener

---
+++ testApps/examples/MongoDbListenerExample/MongoDbListener/RequestLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MongoDbListenerExample.MongoDbListener
+{
+    public class RequestLog
+    {
+        public string Id { get; private set; }
+        public DateTime DateTime { get; set; }
+        public string UserAgent { get; set; }
+        public string HttpMethod { get; set; }
+        public string AbsoluteUri { get; set; }
+        public double DurationInMilliseconds { get; set; }
+        public IEnumerable<KeyValuePair<string, string>> RequestHeaders { get; set; }
+        public int StatusCode { get; set; }
+        public IEnumerable<KeyValuePair<string, string>> ResponseHeaders { get; set; }
+        public IEnumerable<LogMessage> Messages { get; set; }
+
+        public RequestLog()
+        {
+            Id = Guid.NewGuid().ToString();
+            RequestHeaders = new List<KeyValuePair<string, string>>();
+            ResponseHeaders = new List<KeyValuePair<string, string>>();
+            Messages = new List<LogMessage>();
+        }
+    }
+}
