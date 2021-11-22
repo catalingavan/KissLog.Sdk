12429a78cb90803e06d03d411226a7fbdaac24c6
Catalin Gavan <catalin.gavan@ef.com>
2021-11-22 16:45:25 +0100

    implemented CustomMongoDbListener listener

---
+++ testApps/examples/MongoDbListenerExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MongoDbListenerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogTrace("Trace log");
+            _logger.LogDebug("Debug log");
+            _logger.LogInformation("Information log");
+            _logger.LogWarning("Warning log");
+            _logger.LogError("Error log");
+            _logger.LogCritical("Critical log");
+
+            return View();
+        }
+    }
+}
