6222f8ba32bc4b211002ab697a12eceef52009c8
Catalin Gavan <catalingavan@gmail.com>
2021-11-25 08:35:30 +0100

    Entityframework validation example (#52)
    * implemented EntityFramework validation example

---
+++ testApps/examples/EntityFrameworkValidationExample/EntityFramework/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkValidationExample.Models;
+using System.Data.Entity;
+
+namespace EntityFrameworkValidationExample.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(string nameOrConnectionString) :
+            base(nameOrConnectionString)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductConfiguration());
+        }
+    }
+}
---
+++ testApps/examples/EntityFrameworkValidationExample/EntityFramework/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkValidationExample.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace EntityFrameworkValidationExample.EntityFramework
+{
+    internal class ProductConfiguration : EntityTypeConfiguration<Product>
+    {
+        public ProductConfiguration()
+        {
+            HasKey(p => p.Id);
+
+            Property(p => p.Name).IsRequired().HasMaxLength(256);
+            Property(p => p.Code).IsRequired().HasMaxLength(6);
+            Property(p => p.Description).IsRequired().HasMaxLength(2000);
+
+            ToTable("Product");
+        }
+    }
+}
