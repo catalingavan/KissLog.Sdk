617fabc0c3aa9f152ce89522de357418d451b37e
Catalin Gavan <catalingavan@gmail.com>
2024-08-12 14:09:38 +0200

    updated api keys

--- testApps/NLog_ConsoleApp_NetFramework/Program.cs
+++ testApps/NLog_ConsoleApp_NetFramework/Program.cs
@@ -36,9 +36,9 @@ namespace NLog_ConsoleApp_NetFramework
             };
 
             KissLogConfiguration.Listeners
-                .Add(new RequestLogsApiListener(new Application(ConfigurationManager.AppSettings["KissLog.OrganizationId"], ConfigurationManager.AppSettings["KissLog.ApplicationId"]))
+                .Add(new RequestLogsApiListener(new Application(ConfigurationManager.AppSettings["LogBee.OrganizationId"], ConfigurationManager.AppSettings["LogBee.ApplicationId"]))
                 {
-                    ApiUrl = ConfigurationManager.AppSettings["KissLog.ApiUrl"],
+                    ApiUrl = ConfigurationManager.AppSettings["LogBee.ApiUrl"],
                     UseAsync = false
                 });
         }
